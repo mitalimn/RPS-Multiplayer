675261a75362456a633d72d0daa94faee94f7537
mitalimn <mitalinaik@yahoo.com>
2016-11-16 09:15:10 -0600

    desired database tree structure for player 1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,65 @@
-$(document).ready(function(){
- var config = {
-    apiKey: "AIzaSyByI5q3zTJSUh0efLiNDuAh_z4E_WeCp6Y",
-    authDomain: "rps-multiplayer-135f3.firebaseapp.com",
-    databaseURL: "https://rps-multiplayer-135f3.firebaseio.com",
-    storageBucket: "rps-multiplayer-135f3.appspot.com",
-    messagingSenderId: "287006667182"
+
+  var config = {
+    apiKey: "AIzaSyBxhrkAQBw_1WkHFGGFOoOUYhrIWtISsGM",
+    authDomain: "rps-multiplayer-73560.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-73560.firebaseio.com",
+    storageBucket: "rps-multiplayer-73560.appspot.com",
+    messagingSenderId: "507729091215"
   };
   firebase.initializeApp(config);
-
 // var database = firebase.database();
+// // var database = firebase.database();
 
-	$('#add-player').on("click", function(){
-			var playerName = $("#pName").val().trim();
 
-			ref = new Firebase("https://rps-multiplayer-135f3.firebaseio.com/");
+//create different refs for chat and game 
 
-			ref.once("value" , function(snapshot){
-				var p1 =snapshot.child(players).child('1').exists();
-				var p2 =snapshot.child(players).child('2').exists();
+//Initial variables
+//var url = 'https://rps-multiplayer-135f3.firebaseio.com/';
+//You can reference the root, or child location in your 
+//database by calling firebase.database().ref() or child path 
+var database = firebase.database().ref();
 
-				$('#player1').html('playerName');
-			});
+var name = "";
+var comment = "";
+var player1Exists = false;
+var player2Exists = false;
+
+var choice = "";
+var player1Wins = 0;
+var player2Wins = 0;
+var player2losses = 0;
+var player1losses = 0;
+var ties = 0;
+var player1 = "";
+var player2 = "";
+
+var choice1= "";
+var choice2 = "";
+
+
+
+//This creates root player and child as 1
+$(document).ready(function(){
+	database.child("player/1").on("child_added", function(snapshot){
+		if(snapshot.key == "name"){
+			$("#player1").html(snapshot.val());
+		}
+	}, function (error){
+		console.log("Read failed" +error.code);
 	});
-});//.ready function
+//once addplayer button is clicked 
 
-var url = "https://rps-multiplayer-135f3.firebaseio.com/";
-var players = "players";
-var player1 = "1";
-var player2 = "2";
-var turns = 1;
-var wins = 0;
-var losses = 0;
-
-function play(){
-	var playerCount =0;
-
-
-	ref.on("value", function(snapshot){
-
+$('#add-player').on("click", function(){
+	player1 = $('#pName').val().trim();
+	database.update({
+		"player/1/name" : player1,
+		"player/1/choice" : choice1,
+		"player/1/wins" : player1Wins,
+		"player/1/losses" : player1losses
 	});
 
-}
+});
+
+
+
+});//document.ready
