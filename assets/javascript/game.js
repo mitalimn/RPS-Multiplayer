b60546736bb0d31ed8bf7193ba923ce6d0476baa
mitalimn <mitalinaik@yahoo.com>
2016-11-16 10:51:37 -0600

    player2 gets overidden

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,12 @@
-
-  var config = {
+var config = {
     apiKey: "AIzaSyBxhrkAQBw_1WkHFGGFOoOUYhrIWtISsGM",
     authDomain: "rps-multiplayer-73560.firebaseapp.com",
     databaseURL: "https://rps-multiplayer-73560.firebaseio.com",
     storageBucket: "rps-multiplayer-73560.appspot.com",
     messagingSenderId: "507729091215"
-  };
-  firebase.initializeApp(config);
-// var database = firebase.database();
-// // var database = firebase.database();
-
-
-//create different refs for chat and game 
-
-//Initial variables
-//var url = 'https://rps-multiplayer-135f3.firebaseio.com/';
-//You can reference the root, or child location in your 
-//database by calling firebase.database().ref() or child path 
+};
+firebase.initializeApp(config);
+ 
 var database = firebase.database().ref();
 
 var name = "";
@@ -33,33 +23,58 @@ var ties = 0;
 var player1 = "";
 var player2 = "";
 
-var choice1= "";
+var choice1 = "";
 var choice2 = "";
-
+var player = true; //flag
 
 
 //This creates root player and child as 1
-$(document).ready(function(){
-	database.child("player/1").on("child_added", function(snapshot){
-		if(snapshot.key == "name"){
-			$("#player1").html(snapshot.val());
-		}
-	}, function (error){
-		console.log("Read failed" +error.code);
-	});
-//once addplayer button is clicked 
+$(document).ready(function() {
+    database.child("player/1").on("child_added", function(snapshot) {
+        if (snapshot.key == "name") {
+            $("#player1").html(snapshot.val());
+        }
+    }, function(error) {
+        console.log("Read failed" + error.code);
+    });
+  
+    //To get the second player
+    database.child("player/2").on("child_added" , function(snapshot){
+    	if(snapshot.key == "name"){
+    		$("#player2").html(snapshot.val());
+    	}
+    }, function(error){
+    	console.log("Read failed" + error.code);
+    });
 
-$('#add-player').on("click", function(){
-	player1 = $('#pName').val().trim();
-	database.update({
-		"player/1/name" : player1,
-		"player/1/choice" : choice1,
-		"player/1/wins" : player1Wins,
-		"player/1/losses" : player1losses
-	});
+      //once addplayer button is clicked 
 
-});
+    $('#add-player').on("click", function() {
+       if (player === true) {
+       	   console.log("player flag " + player);
+           player1 = $('#pName').val().trim();
+            player =false;
+           database.update({
+               "player/1/name": player1,
+               "player/1/choice": choice1,
+               "player/1/wins": player1Wins,
+               "player/1/losses": player1losses
+               
+           });
+          
+           console.log("player flag " + player);
+       } else {
+       	
+       	  console.log("player flag " + player);
+           player2 = $('#pName').val().trim();
+           database.update({
+               "player/2/name": player2,
+               "player/2/choice": choice2,
+               "player/2/wins": player2Wins,
+               "player/2/losses": player2losses
+           });
+       } //else
+   });
 
 
-
-});//document.ready
+}); //document.ready
